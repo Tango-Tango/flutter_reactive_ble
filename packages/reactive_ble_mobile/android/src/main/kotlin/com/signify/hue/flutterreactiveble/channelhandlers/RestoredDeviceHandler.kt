465b2bbb59add17a639f9d767b8d39501e9276e1
taylorbell <taylorbell.home@gmail.com>
2025-01-07 08:18:43 -0600

    chore: additional formatting fixes

--- packages/reactive_ble_mobile/android/src/main/kotlin/com/signify/hue/flutterreactiveble/channelhandlers/RestoredDeviceHandler.kt
+++ packages/reactive_ble_mobile/android/src/main/kotlin/com/signify/hue/flutterreactiveble/channelhandlers/RestoredDeviceHandler.kt
@@ -3,7 +3,7 @@ package com.signify.hue.flutterreactiveble.channelhandlers
 import io.flutter.plugin.common.EventChannel
 import com.signify.hue.flutterreactiveble.ProtobufModel as pb
 
-class RestoredDeviceHandler() : EventChannel.StreamHandler {
+class RestoredDeviceHandler : EventChannel.StreamHandler {
     private var sink: EventChannel.EventSink? = null
 
     override fun onListen(
@@ -16,7 +16,6 @@ class RestoredDeviceHandler() : EventChannel.StreamHandler {
             val message = pb.RestoredDeviceInfoCollection.newBuilder().build()
             eventSink.success(message.toByteArray())
         }
-
     }
 
     override fun onCancel(objectSink: Any?) {
